fa45145e6360d3f931036fba4b72afed32689d14
Trojan0523 <1153532327@qq.com>
2020-09-05 11:35:28 +0800

    新增 user表中user字段和password_digest字段,数据校验和国际化功能

--- app/controllers/first_controller.rb
+++ app/controllers/first_controller.rb
@@ -1,1 +1,13 @@
+class FirstController < ApplicationController
+  def hello
+    # render plain: 'Hello Trojan'
+    # render json: {name: 'trojan', age: '18'}
+
+    @xxx = 'controller 里面的 @ 变量'
+    render 'first/hello'
+  end
+  def hi
+    render 'first/h1'
+    end
+  end
 
