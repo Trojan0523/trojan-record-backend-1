f1be479ba07b122d7c8ce1e06e2da71fe30d05ae
Trojan0523 <1153532327@qq.com>
2020-09-05 20:09:06 +0800

    创建sessions Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@
 class UsersController < ApplicationController
   def create
     render_resource User.create create_params
-
   end
 
   def create_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get '/hello', to: 'first#hello'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/hi', to: 'first#hi'
+
   resources :users
+  resources :sessions, only: [:create, :destroy]
 
 end
