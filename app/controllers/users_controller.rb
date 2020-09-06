896c0e17e21e76d9c5af9e400177029192f56b62
Trojan0523 <1153532327@qq.com>
2020-09-06 11:23:53 +0800

    获取当前用户信息

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
-
 class UsersController < ApplicationController
   def create
     render_resource User.create create_params
   end
 
+  def me
+    render_resource current_user
+  end
+
   def create_params
     params.permit(:email, :password, :password_confirmation)
   end
-
 end
