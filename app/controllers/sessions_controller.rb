6dd940c7a4e34ac4b8338399b47a8a828f9eac44
Trojan0523 <1153532327@qq.com>
2020-09-06 10:43:16 +0800

    密码不匹配

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,14 @@
 class SessionsController < ApplicationController
   def create
-      s = Session.new create_params
-      s.validate
-      render_resource s
+    s = Session.new create_params
+    s.validate
+    render_resource s
+    # session[:current_user_id] = session.user.id
   end
-    def destroy
+  def destroy
 
-    end
-
-    def create_params
-      params.permit(:email, :password)
-    end
   end
-
+  def create_params
+    params.permit(:email, :password)
+  end
+end
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,20 +1,28 @@
 class Session
   include ActiveModel::Model
-  attr_accessor :email, :password
+  attr_accessor :email, :password, :user
 
   validates :email, presence: true
   validate :check_email, if: :email
+
   validates :password, presence: true
+  validate :email_password_match, if: Proc.new {|s| s.email.present? and s.password.present?}
 
   validates_format_of :email, with: /.+@.+/, if: :email
   validates :password, length: {minimum: 6}, if: :password
 
 
   def check_email
-    user = User.find_by email: email
+    user ||= User.find_by email: email
     if user == nil
       errors.add :email, :not_found
 
+    end
+  end
+  def email_password_match
+    user ||=  User.find_by_email email
+    if user and not user.authenticate(password)
+      errors.add :password, :missmatch
     end
   end
 end
